315234a46baa3022f6625fe6bf03a2d1151eae95
vvsatya <vvsatya@yahoo.com>
2012-06-10 21:34:07 -0700

    Updated route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Predicter::Application.routes.draw do
-  get "home/index"
-
+  #get "home/index"
+  root :to => "home#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
