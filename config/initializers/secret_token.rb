acf17c2bf74d61f74ad4f59690f8645253014572
Satya <vvsatya@yahoo.com>
2012-06-11 03:48:18 +0000

    init files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Predicter::Application.config.secret_token = '57e17c3fe84b44a33c15e3d9622f7620d7661fbaf45b941d858918860f257d0c2ad41d80e6b71621f015074c140a652d5bc1276c3ca8d939a86a4c8704d42c9d'
