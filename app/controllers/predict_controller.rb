21925f0e19858d2e7c5b2e76bcd20c1763ef2864
vvsatya <vvsatya@yahoo.com>
2012-06-10 22:47:54 -0700

    Update controller for JASON view

--- app/controllers/predict_controller.rb
+++ app/controllers/predict_controller.rb
@@ -2,6 +2,7 @@ class PredictController < ApplicationController
   def init
     session[:model] = params[:model]
     @model_id = session[:model] 
+    format.json render :partial => "predict/init.json"
   end
 
   def run
